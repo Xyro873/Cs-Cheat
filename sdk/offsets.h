ced557f904e836c7eb1d9e6423bdeec703f7d8c9
Xyro873 <xyromax873@gmail.com>
2026-05-05 21:15:03 +0200

    Add files via upload

---
+++ sdk/offsets.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace cs2_dumper {
+namespace offsets {
+namespace client_dll {
+    constexpr ptrdiff_t dwEntityList = 0x19F6FA0;
+    constexpr ptrdiff_t dwLocalPlayerController = 0x1A31EF8;
+    constexpr ptrdiff_t dwLocalPlayerPawn = 0x183D898;
+    constexpr ptrdiff_t dwViewMatrix = 0x1A586D0;
+    constexpr ptrdiff_t dwViewAngles = 0x1E4F5A0;
+}  // namespace client_dll
+namespace buttons {
+    constexpr ptrdiff_t attack = 0x1E41C40;
+}  // namespace buttons
+}  // namespace offsets
+}  // namespace cs2_dumper
+

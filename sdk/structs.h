1472da320d48db8a2d320467bc4c5cf9237136ee
Xyro873 <xyromax873@gmail.com>
2026-05-05 21:14:50 +0200

    Create structs.h

---
+++ sdk/structs.h
@@ -0,0 +1,14 @@
+// sdk/structs.h
+#pragma once
+
+// Hier kommen spielspezifische Strukturen
+// Ohne Spiel-Info kann ich nur leere Platzhalter geben
+
+struct Vector3 {
+    float x, y, z;
+};
+
+struct Player {
+    // Platzhalter
+    char pad[0x100];
+};
